d5c786ec44724a72d02181e5b3299b6f434fc246
Elder Frog <sdwwh2259253@gmail.com>
2024-09-01 22:11:25 +0800

    feat: add verification

--- script/shield-layer-minting.sh
+++ script/shield-layer-minting.sh
@@ -7,3 +7,9 @@ forge create \
     --etherscan-api-key GS477KS4QNQTUUEHZSWP1UIBFAYDHWU9GV \
     --verify \
     --constructor-args "0x1481165718D57D8Caed98ad773665F394b456962" "2000000000000000000000000" "2000000000000000000000000"
+
+forge verify-contract \
+    --chain-id 11155111 \
+    --etherscan-api-key GS477KS4QNQTUUEHZSWP1UIBFAYDHWU9GV \
+    --compiler-version "0.8.19+commit.7dd6d404" \
+    --constructor-args $(cast abi-encode "constructor(address, uint256, uint256)" "0x1481165718D57D8Caed98ad773665F394b456962" "2000000000000000000000000" "2000000000000000000000000") "0x000d9035ABFdb56A8F916d94Da02E6d765e56F6D" "ShieldLayerMinting"
--- script/slusd.sh
+++ script/slusd.sh
@@ -7,3 +7,9 @@ forge create \
     --etherscan-api-key GS477KS4QNQTUUEHZSWP1UIBFAYDHWU9GV \
     --verify \
     --constructor-args "0x04c17bc9c98c9cdeddfee8204a0153fe65997db7"
+
+forge verify-contract \
+    --chain-id 11155111 \
+    --etherscan-api-key GS477KS4QNQTUUEHZSWP1UIBFAYDHWU9GV \
+    --compiler-version "0.8.19+commit.7dd6d404" \
+    --constructor-args $(cast abi-encode "constructor(address)" "0x04c17Bc9C98c9cdEddfEe8204a0153Fe65997DB7") "0x1481165718D57D8Caed98ad773665F394b456962" "slUSD"
